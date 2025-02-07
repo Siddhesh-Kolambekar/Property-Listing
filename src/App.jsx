bfe944aa1767c4d85b8fda894a496ddf289d53a5
sweety kolambekar <siddkol04@gmail.com>
2025-02-07 14:22:49 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Home from "./components/Home";
-import Parent from "./components/common/Parent";
-import Property from "./components/Property";
+import Home from "./After SignUp/Home";
+import Parent from "./After SignUp/common/Parent";
+import Property from "./After SignUp/Property";
 
 const App = () => {
   return <Parent />;
